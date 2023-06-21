646914ae1508d330f86d833f1bd77a60055a1fc1
RedondoWilliam <137005448+RedondoWilliam@users.noreply.github.com>
2023-06-21 13:05:09 -0500

    Have been edited  Book_Practice/BookPractice_Chap_1_Pag_4_Section_1_1_4_Writing_and_Running_Your_First_Python_Program.py file, today 21/06/23

--- Book_Practice/BookPractice_Chap_1_Pag_4_Section_1_1_4_Writing_and_Running_Your_First_Python_Program.py
+++ Book_Practice/BookPractice_Chap_1_Pag_4_Section_1_1_4_Writing_and_Running_Your_First_Python_Program.py
@@ -8,4 +8,36 @@ Created on Wed Jun 21 12:28:28 2023
 #  1.1.4 Writing and Running Your First Python Program
 # Pag 4
 
+
+
+# ----------------------------------------------------
+# Name requiered in book for file: ball1.py
+
+
+# Fórmula para el movimiento en caida libre 
+
 print(5*0.6 - 0.5*9.81*0.6**2)
+
+print(1*0.1 - 0.5*9.81*0.1**2)
+
+# ----------------------------------------------------
+# Name requiered in book for file: ball12.py
+
+# Fórmula para el movimiento en caida libre 
+# using variables:: 
+v0 = 5          #initial velocity
+g = 9.81        #gravity aceleration
+t = 0.6         #time
+y = v0*t - 0.5*g*t**2   #position
+print("this is y with variables", y)
+
+
+# ---------------------------------------------------
+# 1.1.11 Formatting Text and Numbers
+
+print("At t =%g s, the height of the ball is %.2f m." %(t,y))
+
+
+
+
+# ---------------------------------------------------
